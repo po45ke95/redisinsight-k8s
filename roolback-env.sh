4d15a9b77985c6dbe2190f1de832858b2a42a012
alex.chang <alex.chang@sensestarl.live>
2021-11-12 11:14:16 +0800

    fix roolback.sh and redisinsightv1.yaml value.

--- roolback-env.sh
+++ roolback-env.sh
@@ -1,4 +1,4 @@
 kubectl delete deployment redisinsight -n default
 kubectl delete svc redisinsight -n default
 kubectl delete ing redisinsight -n default
-kubectl delete backendconfig redisinsight -n default
+kubectl delete backendconfig redisinsight-backendconfig -n default
